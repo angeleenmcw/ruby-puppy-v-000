b109cd06603857355497d5b7bc05da9e86b21f2a
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-20 07:33:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each {|dog_name| puts dog_name.name}
+  end
+  
+  def self.clear_all
+    @@all []
+  end
+  
+end
